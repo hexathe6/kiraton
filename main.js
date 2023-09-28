637ad151731932b0e8f896ec5cc76f21df1baaa6
tedz <>
2023-09-27 23:22:55 -0500

    second day: added node halos, with support for other halos, and halo toggling. halo colors are controlled through css this time instead of being hardcoded

--- main.js
+++ main.js
@@ -22,6 +22,14 @@ var buildings = [];
 var _buildings_graphics = [];
 // format: array of elements or objects containing elements
 
+// lists of 'halo's (colored rings around buildings that indicate range)
+var halos = {};
+// format: {type: [list of halos], ...}
+
+// visibility of each type of halo
+var halos_on = {};
+// format: {type: true/false, ...}
+
 // create an svg element (DRYer than otherwise)
 function createsvgel(t)
 {
@@ -62,11 +70,59 @@ function _createbuilding_graphical(type,pos)
 {
   if(type == "node")
   {
+    let g = createsvgel("g");
+    g.classList.add("group_building_node");
+    c.append(g);
+    let xh = createsvgel("ellipse");
+    g.append(xh);
+    xh.classList.add("building_node_halo");
+    setAttributes(xh,{cx: pos.x, cy: pos.y, rx: 200, ry: 200});
+    halos.building_node.push(xh);
     let x = createsvgel("ellipse");
-    c.append(x);
-    x.classList.add("building-node");
+    g.append(x);
+    x.classList.add("building_node");
     setAttributes(x,{cx: pos.x, cy: pos.y, rx: 20, ry: 20});
+    x.addEventListener("click",() => {toggle_halos("building_node");});
   }
+}
+
+function toggle_halos(type)
+{
+  halos_on[type] = !halos_on[type];
+  let t1 = ["building_node"];
+  if(t1.includes(type))
+  {
+    for(let el of halos[type])
+    {
+      el.setAttribute("opacity", halos_on[type] ? "100%" : "0%");
+    }
+  }
+}
+
+// svg defs element
+var cd;
+
+// graphics init
+function _init_graphical()
+{
+  cd = createsvgel("defs");
+  c.append(cd);
+
+  // node 'halo' gradient
+  {
+    let g = createsvgel("radialGradient");
+    g.id = "building_node_grad_halo";
+    cd.append(g);
+    let s = createsvgel("stop");
+    setAttributes(s,{offset: "0%"});
+    g.append(s);
+    s = createsvgel("stop");
+    setAttributes(s,{offset: "100%"});
+    g.append(s);
+  }
+
+  // halo types
+  halos.building_node = [];
 }
 
 // main init
@@ -77,8 +133,11 @@ function init()
   document.addEventListener("mouseup",(e) => {mouse_is_down = false;});
 
   c = document.getElementById("c");
+  _init_graphical();
 
+  halos_on.building_node = true;
+  
   createbuilding("node", {x: 500, y: 500});
-  createbuilding("node", {x: 450, y: 500});
+  createbuilding("node", {x: 200, y: 500});
 }
 
